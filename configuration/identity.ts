330e574628af574c854af991243a1d73e6a6c331
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-17 15:18:50 -0500

    Update identity.ts
    Changed name and description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Sweet Kay`;
+export const OWNER_DESCRIPTION: string = `Opeyemi Kayode's Chat Bot`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
