83f8ca9ef4af4a2c97f6741d73254f6675f1c157
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-17 15:24:51 -0500

    Update identity.ts
    Updated Identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `Sweet Kay`;
-export const OWNER_DESCRIPTION: string = `Opeyemi Kayode's Chat Bot`;
+export const OWNER_NAME: string = `Opeyemi Kayode`;
+export const OWNER_DESCRIPTION: string = `UNC Kenan-Flagler MBA Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Sweet Kay`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
