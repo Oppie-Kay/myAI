76c50e092d706141173a7c0db4bee3c7bd8e3917
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-26 17:19:59 -0500

    Update streaming.ts
    Updated output tone

--- actions/streaming.ts
+++ actions/streaming.ts
@@ -54,7 +54,7 @@ export async function handleOpenAIStream({
   const startTime = Date.now();
   const streamedResponse = await client.chat.completions.create({
     model: model_name,
-    messages: [{ role: "system", content: "Use document-based knowledge if available. If not, seamlessly provide an answer using general knowledge, without mentioning the source."}, ...messages],
+    messages: [{ role: "system", content: "Use document-based knowledge if available and mention the source. If not, seamlessly provide an answer using general knowledge."}, ...messages],
     stream: true,
     temperature,
   });
