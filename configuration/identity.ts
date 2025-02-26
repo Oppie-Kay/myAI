2236d0ee1423a9cdb1c5a43d934e69d65b4d575f
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-25 21:35:45 -0500

    Update identity.ts
    Updated AI_tone and AI_Role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Opeyemi Kayode`;
 export const OWNER_DESCRIPTION: string = `UNC Kenan-Flagler MBA Student`;
 
 export const AI_NAME: string = `Sweet Kay`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Casual`;
+export const AI_ROLE: string = `Seasoned Chef`;
