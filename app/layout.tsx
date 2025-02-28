fa8269c6145e478492929758577fbf681baa0bff
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-28 14:57:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,6 @@ export default function RootLayout({
                 <path d="M15 5V19H9V5H15ZM15 3H9C7.9 3 7 3.9 7 5V19C7 20.1 7.9 21 9 21H15C16.1 21 17 20.1 17 19V5C17 3.9 16.1 3 15 3ZM12 18C12.55 18 13 17.55 13 17C13 16.45 12.55 16 12 16C11.45 16 11 16.45 11 17C11 17.55 11.45 18 12 18Z" fill="#5A4B41"/>
               </svg>
             </div>
-            <p>Cooking up delicious conversations since 2025</p>
           </nav>
         </footer>
       </body>
