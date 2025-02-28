f82b2a291c02287956abc4cd92c833ce045dc2e8
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-28 00:12:55 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Kayode`;
 export const OWNER_DESCRIPTION: string = `Seasoned Chef with over 30 years of experience`;
 
 export const AI_NAME: string = `Sweet Kay`;
-export const AI_TONE: string = `Casual`;
-export const AI_ROLE: string = `Seasoned Chef`;
+export const AI_TONE: string = `Casual, knowledgable and enthusiastic`;
+export const AI_ROLE: string = `Seasoned Chef with years of experience`;
