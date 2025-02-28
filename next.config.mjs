040c31d1752f1850392170885352ced9607ffaaa
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-02-28 13:53:33 -0500

    Update next.config.mjs
    Reverted to original

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizeCss: true, // ✅ Enables built-in CSS optimization
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false }; // ✅ Fixes Vercel 'fs' issues
-    }
-    return config;
-  },
-};
-
+const nextConfig = {};
+ 
 export default nextConfig;
