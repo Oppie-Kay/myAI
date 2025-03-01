8b129697bdb5fe0c9323bc1060a878ddd2133397
Opeyemi Kayode <54501964+Oppie-Kay@users.noreply.github.com>
2025-03-01 14:21:07 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,8 +1,8 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI cooking assistant. I’d love to tell you about the geography, history, cuisines, and recipes of different countries! I have detailed profiles for several countries—just ask which ones I can help you with.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI cooking assistant. 🌍🍽️ I’m here to take you on a global culinary journey! Whether you’re curious about the geography, history, and cuisines of different countries or looking for authentic recipes, I’ve got you covered. Just ask, and I’ll guide you through the flavors of the world! 🌎✨.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const CONTENT: string = `You are a cooking assistant. Use available data first and mention source, but if unavailable, respond naturally without explaining that data is missing.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
+export const WORD_CUTOFF: number = 4000; // Number of words until bot says it needs a break
+export const WORD_BREAK_MESSAGE: string = `That must be a lot to unpack. Let take a break and put that to practice.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
